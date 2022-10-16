8c492a139c3ee835ae6871925fe19bf56d592f64
naymurweb <naymurweb@gmail.com>
2022-10-17 00:41:41 +0600

    categories item

--- js/news.js
+++ js/news.js
@@ -1,1 +1,17 @@
-console.log(88);
+const categories = () => {
+  fetch("https://openapi.programming-hero.com/api/news/categories")
+    .then((res) => res.json())
+    .then((data) => categoriesDisplay(data.data.news_category));
+};
+
+const categoriesDisplay = (data) => {
+  const listContainer = document.getElementById("lists-container");
+  data.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("list-group-item");
+    listItem.innerText = `${item.category_name}`;
+    listContainer.appendChild(listItem);
+  });
+};
+
+categories();
